927ff867fe3f62d502e064656c586345da9395ad
bstelly <bstelly1999@gmail.com>
2018-02-06 17:53:02 -0600

    Worked on get_neighbor function

--- a_star.py
+++ a_star.py
@@ -3,3 +3,15 @@
 from graph import Graph
 
 class A_star:
+    def __init__(self, graph, start, end):
+        self.grid = graph
+        self.open_list = []
+        self.closed_list = []
+        self.start_node = start
+        self.end_node = end
+        self.current_node = start
+        self.open_list.append(start)
+
+    def get_neighbors(self):
+        
+

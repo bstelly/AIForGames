ac6fb3e5328da5a1b06d1c009fe849d096ace981
bstelly <bstelly1999@gmail.com>
2018-02-27 12:20:01 -0600

    Cleaned up code

--- a_star.py
+++ a_star.py
@@ -2,8 +2,6 @@
 #pylint: disable = E1101
 from operator import attrgetter
 from graph import Graph
-from vector2 import Vector2
-from node import Node
 
 class AStar:
     def __init__(self, graph, start, end):
@@ -22,9 +20,7 @@ class AStar:
         the current node gets added to the closed list and then removed from the open list'''
         self.current_node = min(self.open_list, key=attrgetter('f_score'))
         self.closed_list.append(self.current_node)
-        index = self.open_list.index(min(self.open_list, key=attrgetter('f_score')))
-        self.open_list.pop(index)
-
+        self.open_list.remove(self.current_node)
     def find_path(self):
         '''Function to generate a path from start to end node'''
         self.open_list.append(self.start_node)
@@ -33,19 +29,27 @@ class AStar:
             neighbors = self.grid.get_neighbors(self.current_node)
             for neighbor in neighbors:
                 if (neighbor.is_traversable and not self.closed_list.__contains__(neighbor)):
+                    #Check to see if neighbor is in open list. If not append it and calculate
+                    #g, h, and f scores, then set the parent of the neigbor as the current node
                     if not self.open_list.__contains__(neighbor):
                         self.open_list.append(neighbor)
                         neighbor.calc_g_score(self.current_node)
                         neighbor.calc_h_score(self.goal_node)
                         neighbor.calc_f_score()
                         neighbor.set_parent(self.current_node)
+                    #If neighbor is already in the open list, it already has a parent, so
+                    #recalculate g score to find better path. Recalculate h and f scores
+                    #incase path changes
                     elif self.open_list.__contains__(neighbor):
                         neighbor.calc_g_score(self.current_node)
                         neighbor.calc_h_score(self.goal_node)
                         neighbor.calc_f_score()
+                    #Check to see if goal node is found, then add it to the closed list
+                    #and set goal node as current
                     if neighbor.position == self.goal_node.position:
                         self.closed_list.append(self.goal_node)
                         self.find_current()
+        #Create the path
         path = []
         while self.current_node.parent:
             path.append(self.current_node)
@@ -57,7 +61,7 @@ class AStar:
         '''Function that will print the path that is found'''
         counter = 0
         for node in self.grid.nodes:
-            if counter is 10:
+            if counter is self.grid.length:
                 print '\n',
                 counter = 0
             if node.is_traversable is False:
@@ -67,7 +71,7 @@ class AStar:
             elif node.is_goal:
                 print '[G]',
             elif self.path.__contains__(node):
-                print '[#]',
+                print '[o]',
             else:
                 print '[ ]',
             counter += 1
--- node.py
+++ node.py
@@ -16,23 +16,27 @@ class Node:
 
     def calc_g_score(self, other):
         '''Calculates the G-Score for a node'''
-        if self.position != other.position:
-            if self.parent is None:
-                if (self.position.x_pos == other.position.x_pos or
-                        self.position.y_pos == other.position.y_pos):
-                    self.g_score = other.g_score + 10
-                else:
-                    self.g_score = other.g_score + 14
-            elif self.parent is not None:
-                tentative_g_score = self.g_score
-                if (self.position.x_pos == other.position.x_pos or
-                        self.position.y_pos == other.position.y_pos):
-                    self.g_score = other.g_score + 10
-                else:
-                    self.g_score = other.g_score + 14
-                if tentative_g_score < self.g_score:
-                    self.g_score = tentative_g_score
-                    self.set_parent(other.parent)
+        #Find the g-Score for a neighbor that has just been discovered
+        if self.parent is None:
+            if (self.position.x_pos == other.position.x_pos or
+                    self.position.y_pos == other.position.y_pos):
+                self.g_score = other.g_score + 10
+            else:
+                self.g_score = other.g_score + 14
+        #If node has already been discovered, it should already have a parent
+        #Check to see if path to current node is faster by using a tentative g-score
+        elif self.parent is not None:
+            tentative_g_score = self.g_score
+            if (self.position.x_pos == other.position.x_pos or
+                    self.position.y_pos == other.position.y_pos):
+                tentative_g_score = other.g_score + 10
+            else:
+                tentative_g_score = other.g_score + 14
+            if tentative_g_score < self.g_score:
+                self.g_score = tentative_g_score
+                self.set_parent(other.parent)
+            else:
+                self.g_score = self.g_score
 
     def calc_h_score(self, other):
         '''Calculates the H-Score for a node'''

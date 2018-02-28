2eff6d9c01fc029802c42c3a30f52ec8ae4239ea
bstelly <bstelly1999@gmail.com>
2018-02-28 12:55:57 -0600

    Fixed code

--- Graph.py
+++ Graph.py
@@ -5,19 +5,19 @@ from vector2 import Vector2
 class Graph:
     '''Graph Class for storing nodes'''
 
-    def create_nodes(self):
-        '''Fill the Graph with nodes'''
-        for i in range(0, self.length):
-            for j in range(0, self.height):
-                new_node = Node(Vector2(i, j))
-                self.nodes.append(new_node)
-
     def __init__(self, length, height):
         '''Constructor for Graph class'''
         self.length = length
         self.height = height
         self.nodes = []
         self.create_nodes()
+
+    def create_nodes(self):
+        '''Fill the Graph with nodes'''
+        for i in range(0, self.length):
+            for j in range(0, self.height):
+                new_node = Node(Vector2(i, j))
+                self.nodes.append(new_node)
 
     def get_neighbors(self, current_node):
         '''Returns a list of valid neighbor positions'''
